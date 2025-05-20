755bf30ec7e0dcc547f6f0fa1dd3a6f746d398c3
Pravin <pravin.padalkar@techturbine.com>
2025-05-20 15:21:39 +0530

    Created ConfigProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { ConfigProvider } from "antd";
 import "./App.css";
 import MyLayout from "./Components/MyLayout";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MyLayout />} />
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#3DBCA2",
+        },
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MyLayout />} />
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 }
 
