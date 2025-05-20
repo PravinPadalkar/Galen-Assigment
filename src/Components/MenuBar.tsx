3ef65972720d4f53b77efddeb25da3402ccf860d
Pravin <pravin.padalkar@techturbine.com>
2025-05-20 15:40:18 +0530

    Remove Divider and Add icons to dropdown

--- src/Components/MenuBar.tsx
+++ src/Components/MenuBar.tsx
@@ -11,11 +11,10 @@ const MenuBar = () => {
   ];
   return (
     <section className="flex flex-col items-center justify-center py-4 ">
-      <div className="flex gap-2">
+      <div className="flex gap-2 mb-16">
         <h1 className="text-3xl font-bold">Doctors4U</h1>
         <MedicineBoxFilled className="text-3xl" />
       </div>
-      <Divider>Menu</Divider>
       <Menu defaultSelectedKeys={["1"]} mode="inline" className="bg-inherit shadow-none" items={items} />
     </section>
   );
--- src/utility/MyDropDown.tsx
+++ src/utility/MyDropDown.tsx
@@ -1,38 +1,37 @@
+import { LogoutOutlined, MoneyCollectOutlined, UserOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
-import { Button, Dropdown } from "antd";
+import { Avatar, Button, Dropdown } from "antd";
 import { Link } from "react-router";
 
 const MyDropDown = () => {
   const items: MenuProps["items"] = [
     {
-      key: "1",
-      label: "My Account",
-      disabled: true,
-    },
-    {
-      type: "divider",
-    },
-    {
       key: "2",
-      label: "My Profile",
-      extra: "⌘P",
+      label: "Profile",
+      icon: <UserOutlined />,
     },
     {
       key: "3",
       label: "Subscription",
-      extra: "⌘S",
+      icon: <MoneyCollectOutlined />,
     },
     {
       key: "4",
       label: "Logout",
-      extra: "⌘L",
+      icon: <LogoutOutlined />,
     },
   ];
 
   return (
     <Dropdown menu={{ items }} trigger={["click"]}>
       <Link to="#" onClick={(e) => e.preventDefault()}>
-        <Button className="border-0">My Profile</Button>
+        <Button
+          color="primary"
+          className="outline-none border-0 text-lg"
+          icon={<Avatar size="large" icon={<UserOutlined />} />}
+        >
+          Head Doctor
+        </Button>
       </Link>
     </Dropdown>
   );
