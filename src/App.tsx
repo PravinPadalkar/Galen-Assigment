31c1ad97ce93ad1c6d8b2040f896c7f9e928fbe0
Pravin <pravin.padalkar@techturbine.com>
2025-05-23 11:34:32 +0530

    Created Data Provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 import AppLayout from "./Components/AppLayout";
 import ThemeProvider from "./Provider/ThemeProvider";
+import DataProvider from "./Provider/DataProvider";
 
 function App() {
   return (
     <ThemeProvider>
-      <Routes>
-        <Route path="/" element={<AppLayout />} />
-      </Routes>
+      <DataProvider>
+        <Routes>
+          <Route path="/" element={<AppLayout />} />
+        </Routes>
+      </DataProvider>
     </ThemeProvider>
   );
 }
---
+++ src/Provider/DataProvider.tsx
@@ -0,0 +1,107 @@
+import React, { createContext, useState } from "react";
+import {
+  SlotDurationEnum,
+  type BookedSlotsDetailsType,
+  type doctorDetailsType,
+  type DoctorsWeeklyScheduleType,
+} from "../Helper/types";
+
+interface DataContextType {
+  data: string;
+  setData: React.Dispatch<React.SetStateAction<string>>;
+  doctersDetails: doctorDetailsType[];
+  setDoctersDetails: React.Dispatch<React.SetStateAction<doctorDetailsType[]>>;
+  doctorsWeeklySchedule: DoctorsWeeklyScheduleType[];
+  setDoctorsWeeklySchedule: React.Dispatch<React.SetStateAction<DoctorsWeeklyScheduleType[]>>;
+  bookedSlotsDetails: BookedSlotsDetailsType[];
+  setBookedSlotsDetails: React.Dispatch<React.SetStateAction<BookedSlotsDetailsType[]>>;
+}
+export const dataContext = createContext<DataContextType | undefined>(undefined);
+
+const DataProvider = ({ children }: { children: React.ReactNode }) => {
+  const [data, setData] = useState("hello");
+  const [doctersDetails, setDoctersDetails] = useState<doctorDetailsType[]>([
+    {
+      doctorId: "1",
+      doctorName: "Pravin Padalkar",
+      slotDuration: SlotDurationEnum.thirty,
+    },
+  ]);
+  const [doctorsWeeklySchedule, setDoctorsWeeklySchedule] = useState<DoctorsWeeklyScheduleType[]>([
+    {
+      doctorId: "1",
+      dayOfWeek: "SUN",
+      isAvailable: true,
+      slotStartTime: "9.00 AM",
+      slotEndTime: "10.00 AM",
+    },
+    {
+      doctorId: "1",
+      dayOfWeek: "MON",
+      isAvailable: true,
+      slotStartTime: "9.00 AM",
+      slotEndTime: "10.00 AM",
+    },
+    {
+      doctorId: "1",
+      dayOfWeek: "TUS",
+      isAvailable: true,
+      slotStartTime: "9.00 AM",
+      slotEndTime: "10.00 AM",
+    },
+    {
+      doctorId: "1",
+      dayOfWeek: "WED",
+      isAvailable: true,
+      slotStartTime: "9.00 AM",
+      slotEndTime: "10.00 AM",
+    },
+    {
+      doctorId: "1",
+      dayOfWeek: "THU",
+      isAvailable: true,
+      slotStartTime: "9.00 AM",
+      slotEndTime: "10.00 AM",
+    },
+    {
+      doctorId: "1",
+      dayOfWeek: "FRI",
+      isAvailable: true,
+      slotStartTime: "9.00 AM",
+      slotEndTime: "10.00 AM",
+    },
+    {
+      doctorId: "1",
+      dayOfWeek: "SAT",
+      isAvailable: false,
+      slotStartTime: "9.00 AM",
+      slotEndTime: "10.00 AM",
+    },
+  ]);
+  const [bookedSlotsDetails, setBookedSlotsDetails] = useState<BookedSlotsDetailsType[]>([
+    {
+      doctorId: "1",
+      doctorName: "Pravin",
+      date: "21/05/2025",
+      patientName: "sohel khan",
+      bookedSlots: ["10:00 AM", "10:30 AM", "11:30 AM"],
+    },
+  ]);
+  return (
+    <dataContext.Provider
+      value={{
+        data,
+        setData,
+        doctersDetails,
+        setDoctersDetails,
+        doctorsWeeklySchedule,
+        setDoctorsWeeklySchedule,
+        bookedSlotsDetails,
+        setBookedSlotsDetails,
+      }}
+    >
+      {children}
+    </dataContext.Provider>
+  );
+};
+export default DataProvider;
