21c9c547791daf89153de1e15b0ade208d6cf937
Pravin <pravin.padalkar@techturbine.com>
2025-06-03 12:50:13 +0530

    Implemented TimeZone Compatibility in listview page

--- src/Components/ListView.tsx
+++ src/Components/ListView.tsx
@@ -44,7 +44,7 @@ const ListView = () => {
       return {
         key: slot.slotTime + " " + dateItem.date,
         date: dateItem.date,
-        slot: dayjs(dateItem.date, "DD/MM/YYYY").format("Do MMMM YYYY ") + " - " + slot.slotTime,
+        slot: dayjs(dateItem.date).format("Do MMMM YYYY ") + " - " + dayjs(slot.slotTime).format("hh:mm:A"),
         doctor: dateItem.doctorName,
         action: (
           <Button type="primary" style={{ borderRadius: "4px" }}>
@@ -62,10 +62,8 @@ const ListView = () => {
     }
   };
   const filteredList = dataSource
-    .filter((item) => (filterDate ? !item.date.localeCompare(filterDate) : true))
-    .filter((item) =>
-      filterMonth ? !dayjs(item.date, "DD/MM/YYYY").format("MM/YYYY").localeCompare(filterMonth) : true
-    );
+    .filter((item) => (filterDate ? !dayjs(item.date).format("DD/MM/YYYY").localeCompare(filterDate) : true))
+    .filter((item) => (filterMonth ? !dayjs(item.date).format("MM/YYYY").localeCompare(filterMonth) : true));
   return (
     <>
       <section className="min-h-16 flex justify-between items-center mx-8 mt-4 mb-8">
@@ -102,29 +100,6 @@ const ListView = () => {
         scroll={{ x: true }}
         pagination={{ pageSize: 8 }}
       />
-      {/* <div className="flex min-h-16 items-center mx-8 ">
-        <span className="font-bold flex-[1] ">Date & Time</span>
-        <div className="font-bold flex-[3] ">Doctor</div>
-      </div>
-      <Divider className="m-0 mx-8  " />
-      {BookedSlots.length === 0 ? (
-        <div>NO Data</div>
-      ) : (
-        BookedSlots.map((item, i) => (
-          <div key={i} className="flex min-h-14 items-center mx-8 ">
-            <span className="flex-[1] ">{dayjs(item.date, "DD/MM/YYYY").format("DD MMMM ") + " - " + item.slot}</span>
-            <div className="flex-[3] flex items-center justify-between ">
-              <div className="flex gap-2 items-center">
-                <Avatar style={{ backgroundColor: "#87d068" }} icon={<UserOutlined />} />
-                <span>{item.doctor}</span>
-              </div>
-              <Button style={{ borderRadius: "4px" }} type="primary">
-                Start Call
-              </Button>
-            </div>
-          </div>
-        ))
-      )} */}
     </>
   );
 };
